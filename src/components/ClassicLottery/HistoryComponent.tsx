7fea60e7e1724e82ed636391cc2b3c7ebb680bfb
elishadiah <elishadiah326@gmail.com>
2022-11-22 04:46:15 -0500

    style fix: historycomponent

--- src/components/ClassicLottery/HistoryComponent.tsx
+++ src/components/ClassicLottery/HistoryComponent.tsx
@@ -29,33 +29,34 @@ const HistoryComponent = (props: any) => {
       <Box
         {...props}
         w={["100vw", "90vw", "340px"]}
+        h={"480px"}
         borderRadius="10px"
         whiteSpace="nowrap"
         bg={colorMode === "dark" ? "black" : "white"}
         border={"1px"}
         borderColor={colorMode === "dark" ? "white" : "black"}
-        p="10px"
+        p="8px"
         m={["10px"]}
       >
         <Box
           bg={colorMode === "dark" ? "#5C5C5C" : "#E2E2E2"}
           borderRadius="5px"
           border="1px"
-          minH={"450px"}
+          minH={"462px"}
           paddingX="15px"
           borderColor={colorMode === "dark" ? "black" : "black"}
           paddingBottom="150px"
         >
-          <Flex justifyContent="space-evenly" mt="5px">
+          <Flex justifyContent="space-evenly" mt="15px">
             <Text
-              fontSize={"xl"}
+              fontSize={"2xl"}
               pl="6px"
               color={colorMode === "dark" ? "#C5C5C5" : ""}
             >
               ALL HISTORY
             </Text>
             <Text
-              fontSize={"xl"}
+              fontSize={"2xl"}
               pl="6px"
               color={colorMode === "dark" ? "#C5C5C5" : ""}
             >
@@ -64,21 +65,21 @@ const HistoryComponent = (props: any) => {
           </Flex>
           <Container style={{ background: "#F0B90B" }}>
             <h2>PRIZE POOL</h2>
-            <p>
-              99,9999000.00 <br /> <span>-$123456.00</span>
+            <p style={{display: 'block'}}>
+              88,888888.00 <br /> <span>-$123456.00</span>
             </p>
           </Container>
           <Container style={{ background: "#B49EF2" }}>
             <h2>ROUND</h2>
             <h4>{`<`}</h4>
-            <p>123456</p>
+            <p>0000</p>
             <h4>{`>`}</h4>
-            <h3>first</h3>
-            <h3>last</h3>
+            <h3>First</h3>
+            <h3>Last</h3>
           </Container>
           <Container style={{ background: "#37A93B" }}>
-            <h2>LAST WINNER</h2>
-            <p>1234356</p>
+            <h2>WINNER WINNER</h2>
+            <p style={{fontSize: "32px"}}>1234356</p>
           </Container>
         </Box>
       </Box>
@@ -90,9 +91,9 @@ const Container = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  margin: 10px;
+  margin: 10px 8px;
   padding: 10px;
-  min-height: 55px;
+  height: 55px;
 
   border: 1px solid #000000;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
@@ -103,7 +104,6 @@ const Container = styled.div`
     font-weight: 400;
     font-size: 18px;
     line-height: 19px;
-
     color: #000000;
   }
   h3 {
@@ -113,8 +113,10 @@ const Container = styled.div`
     font-size: 20px;
   }
   p {
+    display: flex;
+    align-items: center;
     background: #ffffff;
-    border: 1px solid #f0b90b;
+    border: 1px solid #8E8E8E;
     border-radius: 5px;
     font-family: "Ropa Sans";
     font-style: normal;
@@ -122,15 +124,17 @@ const Container = styled.div`
     font-size: 24px;
     line-height: 26px;
     color: #000000;
-    padding: 8px;
-    max-height: 55px;
+    padding: 0 10px;
+    height: 40px;
     margin-left: 5px;
     span {
-      font-size: 16px;
+      font-size: 12px;
       margin: 0;
       padding: 0;
       position: relative;
-      top: -5px;
+      top: -12px;
+      line-height: 18px;
+      font-weight: 400;
     }
   }
 `;
